d12bbaf7c00e52446862975b8426707710445a4b
Chris Barrett <c.barrett@me.com>
2016-07-11 21:30:16 -0500

    rest of code

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Cheers
             Console.Write(prompt);
             String user_name = Console.ReadLine().ToLower(); //Read from terminal save to a variable
             
+            // First refactoring using "Else"
             // for (int i = 0; i < user_name.Length; i++) {
             // String cheer_prefix = "Give me a"; // Resets the cheer_prefixvariable every iteration through for loop
             //     if (requires_an.IndexOf(user_name[i]) > -1 ) {
@@ -30,8 +31,23 @@ namespace Cheers
 
             // Last Refactoring using Contains instead of 'IndexOf'
             for (int i = 0; i < user_name.Length; i++) {
-                Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
+                if (requires_an.Contains(user_name[i])) {
+                    Console.WriteLine(cheer_prefix + "n.. " + user_name[i]);
+                } else {
+                    Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
+                }
             }
+
+            // Original solution, using "IndexOf"
+            // for (int i = 0; i < user_name.Length; i++) {
+            //     if (requires_an.IndexOf(user_name[i]) > -1 ) {
+            //         // Then it's in the requires_an array
+            //     Console.WriteLine(cheer_prefix + "n.. " + user_name[i]);
+            //     } else {
+            //         // Does not require an "an"
+            //     Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
+            //     }
+            // }
 
             // user_name.ToUpper() is turning the user's name to uppercase.
             // ToUpper() is functionality specific to Strings
